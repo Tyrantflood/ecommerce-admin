127ddd6ff3e020388ab43f4996454effcdaf7ac4
Tyrantflood <princeobi124@gmail.com>
2023-07-05 14:40:04 +0100

    setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button size="default" variant="destructive">
+        {" "}
+        CLick Me
+      </Button>
+    </main>
+  );
+}
