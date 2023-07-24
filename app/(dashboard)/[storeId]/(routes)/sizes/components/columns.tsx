90f5cd7ebaf72e5432f983d9d90b7c74d2ba3e36
Tyrantflood <princeobi124@gmail.com>
2023-07-24 12:08:03 +0100

    reformating billboards to fit sizes

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+
+export type SizeColumn = {
+  id: string;
+  name: string;
+  value: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<SizeColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "name",
+  },
+  {
+    accessorKey: "value",
+    header: "value",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
