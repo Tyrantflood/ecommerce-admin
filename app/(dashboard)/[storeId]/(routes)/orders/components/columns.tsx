e6accf28bcc405142263f6835e60985a824996c9
Tyrantflood <princeobi124@gmail.com>
2023-07-30 03:21:08 +0100

    order model complete

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+export type OrderColumn = {
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  products: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "prodcuts",
+    header: "products",
+  },
+  {
+    accessorKey: "phone",
+    header: "phone",
+  },
+  {
+    accessorKey: "address",
+    header: "address",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total Price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
+  },
+];
